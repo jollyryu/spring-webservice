f295c2c496d95cda05679c43c34ca42e6c092d43
jollyryu <jollyryu@gmail.com>
2018-02-27 17:16:05 +0900

    최초생성

---
+++ src/main/java/com/jollyryu/springwebservice/controller/WebRestController.java
@@ -0,0 +1,12 @@
+package com.jollyryu.springwebservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+    @GetMapping
+    public String hello(){
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/jollyryu/springwebservice/controller/RestControllerTest.java
@@ -0,0 +1,7 @@
+package com.jollyryu.springwebservice.controller;
+
+import static org.junit.Assert.*;
+
+public class RestControllerTest {
+
+}
