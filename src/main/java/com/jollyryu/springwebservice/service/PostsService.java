2a33e4b30904b7bbf040734734393db8aef2c698
jollyryu <jollyryu@gmail.com>
2018-03-03 17:11:35 +0900

    no message

---
+++ src/main/java/com/jollyryu/springwebservice/service/PostsService.java
@@ -0,0 +1,21 @@
+package com.jollyryu.springwebservice.service;
+
+import com.jollyryu.springwebservice.domain.Posts;
+import com.jollyryu.springwebservice.domain.PostsRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public void save(Posts entity){
+        postsRepository.save(entity);
+
+        System.out.println("success");
+    }
+
+}
