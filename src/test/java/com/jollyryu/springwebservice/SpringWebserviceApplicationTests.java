15c7e7b0c9adc09f0129b7c9559199488f3569e0
jollyryu <jollyryu@naver.com>
2018-03-25 22:59:31 +0900

    Update SpringWebserviceApplicationTests.java

--- src/test/java/com/jollyryu/springwebservice/SpringWebserviceApplicationTests.java
+++ src/test/java/com/jollyryu/springwebservice/SpringWebserviceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringWebserviceApplicationTests {
-
+//check
 	@Test
 	public void contextLoads() {
 	}
