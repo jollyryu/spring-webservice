c03343cac6f65fd79e57c294b26012dd3a90d295
jollyryu <jollyryu@gmail.com>
2018-03-25 22:47:04 +0900

    branch commit

--- src/main/java/com/jollyryu/springwebservice/controller/WebRestController.java
+++ src/main/java/com/jollyryu/springwebservice/controller/WebRestController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @Controller
 @AllArgsConstructor
 public class WebRestController {
